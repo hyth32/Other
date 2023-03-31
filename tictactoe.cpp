efd08f0a6f0e5e43b4a1eca7b8fa0f869dbdba3f
hyth32 <79153074+hyth32@users.noreply.github.com>
2023-03-31 08:07:13 +0300

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -16,19 +16,19 @@ void drawBoard() {
 }
 
 bool checkForWin() {
-    // Check rows for win
+    // проверка рядов
     for (int i = 0; i < 3; i++) {
         if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
             return true;
     }
 
-    // Check columns for win
+    // проверка столбцов
     for (int i = 0; i < 3; i++) {
         if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
             return true;
     }
 
-    // Check diagonals for win
+    // проверка диагоналей
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
         return true;
     if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
